8591845b4a2982bbf8f49cc919db5bd85b2c0689
12v <11772383+12v@users.noreply.github.com>
2025-02-21 15:48:04 +0000

    Move to docs for github pages

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 import io
 
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import FileResponse, StreamingResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi.responses import StreamingResponse
 from PIL import Image
 
 from inference_flickr import inference
 
 app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="pwa", html=True), name="static")
 
 
 async def processing_image(file: UploadFile):
@@ -25,8 +22,3 @@ async def upload_image(file: UploadFile = File(...)):
     image_bytes = await file.read()
     image = Image.open(io.BytesIO(image_bytes))
     return StreamingResponse(processing_image(image), media_type="text/plain")
-
-
-@app.get("/")
-async def root():
-    return FileResponse("pwa/index.html")
